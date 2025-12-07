0c5071c6f6285ead72066fcfd95a7087c6cb93fc
Sergey Panteleenko <depstor90@gmail.com>
2025-12-07 22:07:50 +0300

    input-value

--- src/script/main.js
+++ src/script/main.js
@@ -4,40 +4,50 @@ const input = document.querySelector('#taskInput'),
     btn = document.querySelector('#addTaskBtn'),
     tasksBard = document.querySelector('#todoList');
 
+// const generedId = () => {
+
+// }
+
 const todos = [{
     id: 1,
     text: 'Тестовая задача',
-    comleted: false,
+    completed: false,
     createdAt: new Date().toISOString(),
     order: 0,
 }, {
     id: 2,
     text: 'Тестовая задача-2',
-    comleted: false,
+    completed: true,
     createdAt: new Date().toISOString(),
     order: 1,
 }];
+
 const addTask = () => {
     btn.addEventListener('click', () => {
-        todos.unshift({
-            id: 3,
-            text: input.value,
-            comleted: false,
-            createdAt: new Date().toISOString(),
-            order: 0
-        });
-        input.value = '';
-        renderTask();
+        if (input.value.trim() !== '') {
+            todos.unshift({
+                id: 3,
+                text: input.value,
+                completed: false,
+                createdAt: new Date().toISOString(),
+                order: 0
+            });
+            input.value = '';
+            renderTask();
+        } else {
+            console.log('Введите данные')
+        }
+
     })
 };
 const renderTask = () => {
-    tasksBard.innerHTML = todos.map((res, i) => {
+    tasksBard.innerHTML = todos.map(res => {
         return `
-        <li class="task-item" data-id=${i} draggable="true">
+        <li class="task-item" data-id=${res.id} draggable="true">
                     <div class="drag-handle">
                         <i class="fas fa-grip-vertical"></i>
                     </div>
-                    <input type="checkbox" class="task-checkbox" id=${res.id} data-id=${i}>
+                    <input type="checkbox" class="task-checkbox" id=${res.id} data-id=${res.id}>
                     <div class="task-content">
                         <div class="task-text">
                             ${res.text}
